ca870c236a6b82e5fa0fa696b742631ed1cdc55a
Matt <mattmleach@gmail.com>
2019-01-28 18:33:57 -0500

    added socket events

--- server/server.js
+++ server/server.js
@@ -8,8 +8,18 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
+});
+
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);

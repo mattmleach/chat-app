4622ffef7f6f34f3835962cda1b762a1bedef059
Matt Leach <MLeach@symprod.com>
2019-02-04 13:31:51 -0500

    Added first message creation

--- server/server.js
+++ server/server.js
@@ -15,14 +15,8 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
     
-    socket.emit('newEmail', {
-        from: 'matt@example.com',
-        text: 'Hey. Whats up.',
-        createdAt: 123
-    });
-
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
     });
 });
 
